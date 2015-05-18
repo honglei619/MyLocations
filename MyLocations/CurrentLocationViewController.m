50773757b153bd92d779566e1d51669df6da9d33
honglei <honglei_cn@126.com>
2015-05-18 22:15:23 +0800

    Lesson 8
    获取地理位置，并显示在firstVIew 中的label上。

--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -17,6 +17,9 @@
 
 @implementation CurrentLocationViewController{
     CLLocationManager *_locationManager;
+    CLLocation *_location;
+    BOOL _updatingLocation;
+    NSError *_lastLocationError;
 }
 -(id)initWithCoder:(NSCoder *)aDecoder{
     if((self = [super initWithCoder:aDecoder])){
@@ -27,6 +30,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self updateLabels];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -35,21 +39,75 @@
     // Dispose of any resources that can be recreated.
 }
 -(IBAction)getLocaton:(id)sender{
-    _locationManager.delegate = self;
-    _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
-    [_locationManager startUpdatingLocation];
-    
-    NSLog(@"1");
+    [self startLocaltionManager];
+    [self updateLabels];
 }
 #pragma mark -CLLocationManagerDelegate
 
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
     NSLog(@"定位失败:%@",error);
+    if(error.code == kCLErrorLocationUnknown){
+        return;
+    }
+    [self stopLocationManager];
+    _lastLocationError = error;
+    [self updateLabels];
 }
 
 -(void)locationManager:(CLLocationManager*)manager didUpdateLocations:(NSArray *)locations{
     CLLocation *newLocation = [locations lastObject];
     NSLog(@"已更新坐标，当前位置是:%@",newLocation);
+    _lastLocationError = nil;
+    _location = newLocation;
+    [self updateLabels];
+}
+
+-(void)updateLabels{
+    if(_location != nil){
+        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f",_location.coordinate.latitude];
+        self.longtitudeLabel.text = [NSString stringWithFormat:@"%.8f",_location.coordinate.longitude];
+        self.tagButton.hidden = NO;
+        self.messageLabel.text = @"";
+    }else{
+        self.latitudeLabel.text = @"";
+        self.longtitudeLabel.text = @"";
+        self.adderessLabel.text = @"";
+        self.tagButton.hidden = YES;
+        self.messageLabel.text = @"请按按钮开始定位";
+        
+        NSString *statusMessage;
+        if (_lastLocationError != nil) {
+            if([_lastLocationError.domain isEqualToString:kCLErrorDomain]&& _lastLocationError.code == kCLErrorDenied){
+                statusMessage = @"对不起，用户已关闭定位服务";
+            }else{
+                statusMessage = @"对不起，定位失败";
+            }
+        }else if(![CLLocationManager locationServicesEnabled]){
+            statusMessage = @"对不起，用户已关闭定位服务";
+        }else if(_updatingLocation){
+            statusMessage = @"正在获取地址...";
+        }else{
+            statusMessage = @"请按按钮开始定位";
+        }
+        self.messageLabel.text = statusMessage;
+    }
+}
+
+-(void)stopLocationManager{
+    if(_updatingLocation){
+        [_locationManager stopUpdatingLocation];
+        _locationManager.delegate = nil;
+        _updatingLocation = NO;
+    }
+}
+
+-(void)startLocaltionManager{
+    if ([CLLocationManager locationServicesEnabled]) {
+        _locationManager.delegate = self;
+        _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        [_locationManager startUpdatingLocation];
+        _updatingLocation = YES;
+    }
 }
 
 @end
