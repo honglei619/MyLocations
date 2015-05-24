77a171067196e807aaf95e01f8c4099bef7a5aea
honglei <honglei_cn@126.com>
2015-05-24 23:09:50 +0800

    lesson 14

---
+++ MyLocations/LocationDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDetailsViewController.h
+//  MyLocations
+//
+//  Created by Lei on 15/5/21.
+//  Copyright (c) 2015年 honglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailsViewController : UITableViewController
+
+@end
